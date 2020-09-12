a6eaf7e4c7ee13fff348997e609fb01c1355e37e
Pankhudi Bhonsle <pankhubh@thoughtworks.com>
2020-09-12 17:05:43 +0530

    Initialize gin router and serve requests on port 8085

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 
 	fmt.Print("Starting service B")
+
+	r := gin.Default()
+	err := http.ListenAndServe(":8085", r)
+	if err != nil {
+		fmt.Println("Could not start service B", err)
+	}
 }
