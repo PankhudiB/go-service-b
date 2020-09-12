acbc7971ef05fd0249e5b0dc0ae61377cbae23a7
Pankhudi Bhonsle <pankhubh@thoughtworks.com>
2020-09-12 17:22:26 +0530

    Annotate to the span of hello request trace

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"go-service-b/constants"
 	"go-service-b/request"
 	"go-service-b/tracing"
+	"go.opencensus.io/trace"
 	"net/http"
 )
 
@@ -34,11 +35,17 @@ func main() {
 
 func handleRequest(ctx *gin.Context) error {
 	fmt.Println("\n------------------ Welcome to Service B ------------------\n")
+	context := ctx.Request.Context()
+	span := trace.FromContext(context)
+	defer span.End()
+	span.Annotate([]trace.Attribute{trace.StringAttribute("In Service B", "Shhhhh ! you are being monitored !")}, "")
+
 	var req request.HelloBRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		fmt.Println("Error reading request hello B", err.Error())
 		return err
 	}
 	fmt.Printf("%v", req)
+	span.Annotate([]trace.Attribute{trace.StringAttribute("Someone waved hello", fmt.Sprintf("%v", req))}, "serviceBValue annotation check")
 	return nil
 }
